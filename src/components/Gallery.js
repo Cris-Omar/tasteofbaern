2c2cd1c9ed3b0f69d492f331798d0435b6b5d973
Cris-Omar <c-o.hernandez@hotmail.com>
2023-08-11 16:18:19 +0200

    divers mobile device optimation

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -34,7 +34,7 @@ export default function Gallery() {
       autoPlay={true}
       showThumbnails={false}
       showBullets={true}
-      
+      showFullscreenButton={false}
       />
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,42 @@
-import "../styles/style.css" 
-import React, {useState} from "react"
-import { Link } from 'react-router-dom'
-import { useThemeContext } from '../components/context/ThemeContext'
+import "../styles/style.css";
+import React, {useState} from "react";
+import { Link } from 'react-router-dom';
+import { useThemeContext } from '../components/context/ThemeContext';
 import { useTranslation } from 'react-i18next';
 import i18n from "i18next";
-import Moon from "../assets/icons/Moon"
-import Sun from "../assets/icons/Sun"
-import LogoforDark from "../assets/icons/LogoforDark"
-import LogoforLight from "../assets/icons/LogoforLight"
-import MenuXmark from "../assets/icons/MenuXmark"
-import MenuBurger from "../assets/icons/MenuBurger"
+import Moon from "../assets/icons/Moon";
+import Sun from "../assets/icons/Sun";
+import LogoforDark from "../assets/icons/LogoforDark";
+import LogoforLight from "../assets/icons/LogoforLight";
+import MenuXmark from "../assets/icons/MenuXmark";
+import MenuBurger from "../assets/icons/MenuBurger";
 
 export default function NavBar() {
 
-  //change Light Dark Theme
-  const {theme, setTheme } = useThemeContext()
-  
   const { t } = useTranslation();
 
-  //handle buttonclick for Theme change
-  const [ click, setClick] = useState(false)
-
-  //function to change style
-  const handleClick = () => {
+  //change Light Dark/Theme
+  const {theme, setTheme } = useThemeContext();
+  const [ isThemeButtonClicked, setIsThemeButtonClicked] = useState(false);
+  //function to change Light/Dark Theme
+  const handleIsThemeButtonClicked = () => {
     setTheme((state ) => (state === "Dark" ? "Light" : "Dark"))
-    setClick(!click)
+    setIsThemeButtonClicked(!isThemeButtonClicked)
   }
 
+  //Language Chooser
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
-
+  //Language Chooser function
   const chooseLanguage = (e) => {
     e.preventDefault();
     i18n.changeLanguage(e.target.value);   // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
     setSelectedLanguage(e.target.value);
     localStorage.setItem("lang", e.target.value);
-}
+  }
 
   //Mobile Open & close menuIcon
   const [menuClick, setMenuClick] = useState(false)
-  const handleMenuClick = () => setMenuClick(!click)
+  const handleMenuClick = () => setMenuClick(!menuClick)
 
   const [locOpen, setLocOpen] = useState(false)
   const handleLocOpen = () =>  {setLocOpen(!locOpen)}
@@ -75,7 +73,7 @@ export default function NavBar() {
             <option value="es">ES</option>
           </select>
 
-          <button onClick={handleClick}>
+          <button onClick={handleIsThemeButtonClicked}>
             {theme === "Light" ? <Moon /> : <Sun />}
           </button>
         </div>
@@ -83,27 +81,20 @@ export default function NavBar() {
 
       {/*small screen Navigation Menu starts here */}
       <div className="mobNavbarMenu">
-        <div className='logo'> 
-          {theme === "Light" ? (
-              <Link to="/"><LogoforLight /></Link>
-            ) : (
-              <Link to="/"><LogoforDark /></Link>
-            )}
-        </div>
 
         <div className="openCloseIcon" onClick= {() => {handleMenuClick(); handleMobOpen();}} >
-          {menuClick ? <MenuBurger /> : <MenuXmark />}
+          {menuClick ? <MenuXmark /> : <MenuBurger /> }
+
           {mobOpen && (
             <div className='mobSubMenu'>
-
-              <li><Link to="/standorte" >{t("Standorte")}</Link></li>
+              <li><Link to="/">{t("Home")}</Link></li>
               <li><Link to="/offer">{t("Offer")}</Link></li>
               <li><Link to="/events">{t("Events")}</Link></li>
               {/*<li><Link to="/voucher">{t("Voucher")}</Link></li> */}
               <li><Link to="/reservation">{t("Reservation")}</Link></li>
               <li><Link to="/location">{t("Location")}</Link></li>
               <li><Link to="/openinghours">{t("Opening_Hours")}</Link></li>
-              
+
               <select defaultValue={selectedLanguage} onChange={chooseLanguage}>  
                 <option value="en" >EN</option>
                 <option value="de">DE</option>
@@ -111,14 +102,13 @@ export default function NavBar() {
                 <option value="es">ES</option>
               </select>
 
-              <button onClick={handleClick}>
+              <button onClick={handleIsThemeButtonClicked}>
                 {theme === "Light" ? <Moon /> : <Sun />}
               </button>
             </div>
             )}
-          </div>
+        </div>
       </div>
-      {/*small screen Navigation Menu starts here */}
     </div>
   )
 }
