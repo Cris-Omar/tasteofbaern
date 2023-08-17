e223a3be9e9e2a8bf0041f2948b149711987adbc
Cris-Omar <c-o.hernandez@hotmail.com>
2023-08-17 13:12:26 +0200

    supabase renewal, contactform works, dir changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import "../styles/style.css";
-import { useThemeContext } from "../components/context/ThemeContext";
+import '../styles/style.css';
+import { useThemeContext } from '../components/context/ThemeContext';
 import { useTranslation } from 'react-i18next';
 import supabase from '../config/SupabaseClient';
 
@@ -26,26 +26,31 @@ export default function Contact() {
       setSubmitting(true);
       setSubmitError(null);
 
-      const { data, error } = await supabase
-        .from('contactForm')
-        .insert([
-          { name, email, subject, message },
-        ]);
+      const { data, error } = await supabase.from('contactForm').insert([
+        { name, email, subject, message },
+      ]);
 
       if (error) {
         setSubmitError('Error submitting form. Please try again later.');
         console.error('Error submitting form:', error);
       } else {
         console.log('Form submitted successfully:', data);
-        // You can show a success message to the user here
-      }
-
-      if (!error) {
-        setShowConfirmation(true); // Show the confirmation message
+        setMessageText('');
+        setShowConfirmation(true);
         setTimeout(() => {
-          setShowConfirmation(false); // Hide the confirmation message after a delay
-        }, 19000); // Adjust the delay as needed (in milliseconds)
-       }
+          setShowConfirmation(false);
+        }, 19000);
+
+      // Reset form fields
+      e.target.reset();
+    }
+    
+      if (!error) {
+        setShowConfirmation(true); 
+        setTimeout(() => {
+          setShowConfirmation(false); 
+        }, 19000); 
+      }
     } catch (error) {
       setSubmitError('An unexpected error occurred. Please try again later.');
       console.error('Error submitting form:', error);
@@ -54,42 +59,41 @@ export default function Contact() {
     }
   };
 
+  const characterLimit = 600;
+
   return (
-    <div className='contact' id={theme}>
+    <div className="contact" id={theme}>
       <h2>{t('contact_us')}</h2>
       <h3>{t('contact_us_text')}</h3>
 
-      <div className='contactForm'>
-
+      <div className="contactForm">
         <form onSubmit={handleSubmit}>
-          {/* Render error message if there's a submit error */}
-
-          <div className='object'>
+          <div className="object">
             <label>{t('name')}</label>
-            <input type="text" name="name" required maxLength={80}/>
+            <input type="text" name="name" required maxLength={80} />
           </div>
-          <div className='object'>
+          <div className="object">
             <label>{t('email')}</label>
             <input type="email" name="email" required />
           </div>
-          <div className='object'>
+          <div className="object">
             <label>{t('subject')}</label>
-            <input type="text" name="subject" required maxLength={120}/>
+            <input type="text" name="subject" required maxLength={120} />
           </div>
-          
-          <div className='object'>
+
+          <div className="object">
             <label>{t('message')}</label>
             <textarea
               rows={8}
               name="message"
               required
-              maxLength={600}
+              maxLength={characterLimit}
               value={messageText}
               onChange={(e) => setMessageText(e.target.value)}
             />
             <span className="characterCount">
-              {messageText.length || 0} / 600 characters
-              {messageText.length > 600 && (
+              {messageText.length} / {characterLimit} characters
+              {messageText.length > characterLimit && (
                 <span className="characterLimitExceeded"> - Limit Exceeded</span>
               )}
             </span>
@@ -101,11 +105,10 @@ export default function Contact() {
               {t('confirmation_Success_Message')}
             </div>
           ) : null}
-
+          
           <button type="submit" disabled={submitting}>
             {submitting ? t('sending') : t('send')}
           </button>
-  
         </form>
       </div>
     </div>
